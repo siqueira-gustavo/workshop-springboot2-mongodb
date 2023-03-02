0ffee765bc0698850014322aead8e3a24e0283b2
Gustavo Siqueira <guss.ns@gmail.com>
2023-03-02 16:35:40 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/example/gugaguga/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,9 @@
+package com.example.gugaguga.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+  private static final long serialVersionUID = 1L;
+
+  public ObjectNotFoundException(String message) {
+    super(message);
+  }
+}
